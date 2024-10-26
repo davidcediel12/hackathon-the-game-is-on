6c43dead97befc2008b2ad957c49e0dd01a3b531
David Cediel <esdago18@gmail.com>
2024-10-26 16:51:49 +0200

    Handle if a request attribute is invalid

--- src/main/java/com/hackathon/bankingapp/controller/ApiControllerAdvice.java
+++ src/main/java/com/hackathon/bankingapp/controller/ApiControllerAdvice.java
@@ -4,6 +4,7 @@ import com.hackathon.bankingapp.exceptions.ApiException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -19,5 +20,10 @@ public class ApiControllerAdvice {
     @ExceptionHandler(BadCredentialsException.class)
     public ResponseEntity<String> handleBadCredentialsException(BadCredentialsException badCredentialsException) {
         return new ResponseEntity<>("Bad credentials", HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Void> handleMethodArgumentNotValidException(MethodArgumentNotValidException methodArgumentNotValidException) {
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
 }
