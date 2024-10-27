187fce026caf0243780050ca328b101a62db6c7e
David Cediel <esdago18@gmail.com>
2024-10-27 10:44:59 +0100

    Merge previous account controller and user controller into dashboard controller.
    In order to clear the account controller to create the new endpoints

--- src/main/java/com/hackathon/bankingapp/controller/AccountController.java
+++ src/main/java/com/hackathon/bankingapp/controller/AccountController.java
@@ -1,25 +1,13 @@
 package com.hackathon.bankingapp.controller;
 
 
-import com.hackathon.bankingapp.dto.response.AccountDetailResponse;
-import com.hackathon.bankingapp.services.AccountService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/dashboard")
+@RequestMapping("/api/account")
 @RequiredArgsConstructor
 public class AccountController {
 
-
-    private final AccountService accountService;
-
-
-    @GetMapping("/account")
-    public ResponseEntity<AccountDetailResponse> getAccountDetail() {
-        return ResponseEntity.ok(accountService.getLoggedInUserAccount());
-    }
 }
--- src/main/java/com/hackathon/bankingapp/controller/DashboardController.java
+++ src/main/java/com/hackathon/bankingapp/controller/DashboardController.java
@@ -1,6 +1,8 @@
 package com.hackathon.bankingapp.controller;
 
+import com.hackathon.bankingapp.dto.response.AccountDetailResponse;
 import com.hackathon.bankingapp.dto.response.UserDetailsResponse;
+import com.hackathon.bankingapp.services.AccountService;
 import com.hackathon.bankingapp.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -11,12 +13,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/dashboard")
 @RequiredArgsConstructor
-public class UserController {
+public class DashboardController {
 
     private final UserService userService;
+    private final AccountService accountService;
 
     @GetMapping("/user")
     public ResponseEntity<UserDetailsResponse> getUserInfo() {
         return ResponseEntity.ok(userService.getLoggedInUserDetails());
+    }
+
+
+    @GetMapping("/account")
+    public ResponseEntity<AccountDetailResponse> getAccountDetail() {
+        return ResponseEntity.ok(accountService.getLoggedInUserAccount());
     }
 }
