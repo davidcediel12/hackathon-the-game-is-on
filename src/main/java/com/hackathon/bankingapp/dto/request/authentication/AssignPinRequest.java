8f23747fc6093781fcc25c2a55b8917ae99ab5fd
David Cediel <esdago18@gmail.com>
2024-10-28 01:23:26 +0100

    Add missing password validations

--- src/main/java/com/hackathon/bankingapp/dto/request/authentication/AssignPinRequest.java
+++ src/main/java/com/hackathon/bankingapp/dto/request/authentication/AssignPinRequest.java
@@ -1,8 +1,9 @@
 package com.hackathon.bankingapp.dto.request.authentication;
 
+import com.hackathon.bankingapp.utils.Password;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 
 public record AssignPinRequest(@NotBlank @Pattern(regexp = "\\d{4}") String pin,
-                               @NotBlank String password){
+                               @Password String password){
 }
--- src/main/java/com/hackathon/bankingapp/dto/request/authentication/UpdatePinRequest.java
+++ src/main/java/com/hackathon/bankingapp/dto/request/authentication/UpdatePinRequest.java
@@ -1,9 +1,10 @@
 package com.hackathon.bankingapp.dto.request.authentication;
 
+import com.hackathon.bankingapp.utils.Password;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 
 public record UpdatePinRequest(@NotBlank @Pattern(regexp = "\\d{4}") String oldPin,
                                @NotBlank @Pattern(regexp = "\\d{4}") String newPin,
-                               @NotBlank String password) {
+                               @Password String password) {
 }
